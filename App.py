4c58e582334de9859a35a12dc5e66c50b3e7f7f2
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-12-04 20:20:52 +0500

    DATA_TYPES

---
+++ App.py
@@ -0,0 +1,35 @@
+# DATATYPES
+
+# -Number:1234,3.142,3+4j,0b111,Decimal,Fraction()
+
+# String :"ZAIN",'Talah
+# String ko bhi list ki tarah treat kia jata hai jaise hum username main zain likain or username[0] tu wo haiamin z dega
+# string ko hum change nahi karsakhte aise hum likain gay username[0]='b' put karo tu wo nahi jaiga
+# -1 last se countiong shorro kariga
+
+# Ye mutable hoti hai (matlab isko badla ja sakta hai), aur ismein alag-alag type ke data (jaise integers, strin
+#  floats, aur hatta ke dusri lists) ek jagah store kiye ja sakte hain
+
+# myList=[1,{2,'',4].
+# isko array ki 0 se count karainge lis kay under list bhi banasakhte hain
+# array ko hi list kaha gaya hai python main
+
+
+# Tuple:(12,30)
+# Value 0 se start hothi hai
+
+
+# Dictionary:{key,value}
+# ismain value 0 se start nahi hothi   har ek value ki special key day sakhte hain
+
+# set('abc')or{a,b,c}
+
+# File:open('')
+
+# Boolean:true or false
+
+# None:None
+
+# Functions,module,Classes
+
+# Advance:Decorator,Generators,Iterators,MetaProgramming
