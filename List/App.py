3f45455952243d3aa5048f8695d0e583a051232d
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-12-15 19:16:51 +0500

    LIST IN PYTHON

---
+++ List/App.py
@@ -0,0 +1,54 @@
+# # LIST
+# tea_varities=["Black","Green","Oolong","White"]
+# tea_varities[2]="coffee"
+# print(tea_varities)
+
+
+# tea_varities=["Black","Green","Oolong","White"]
+# print(tea_varities[1:3])
+
+
+# tea_varities=["Black","Green","Oolong","White"]
+# tea_varities[1:3]=["Lemon","masala"]
+# print(tea_varities)
+
+
+# loop using list 
+# tea_varities=["Black","Green","Oolong","White"]
+# for tea in tea_varities:
+#     print(tea)
+
+# tea_varities=["Black","Green","Oolong","White"]
+# for tea in tea_varities:
+#     print(tea,end="-")
+
+# append end main value add karta hai
+# tea_varities=["Black","Green","Oolong","White"]
+# tea_varities.append("Coffee")
+# print(tea_varities)
+
+
+# tea_varities=["Black","Green","Oolong","White"]
+# if "Oolong" in tea_varities:
+#     print("I have Oolong tea")
+
+
+# pop se last value delet hothi hai
+
+# tea_varities=["Black","Green","Oolong","White"]
+# tea_varities.pop()
+# print(tea_varities)
+
+# remove method kahain se bhi delete karsaktha hai
+
+# tea_varities=["Black","Green","Oolong","White"]
+# tea_varities.remove("Black")
+# print(tea_varities)
+
+# insert add karna 
+# tea_varities=["Black","Green","Oolong","White"]
+# tea_varities.insert(2,"Coffee")
+# print(tea_varities)
+
+squared_num=[x**2 for x in range(10)]
+print(squared_num)
