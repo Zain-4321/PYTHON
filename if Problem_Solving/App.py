616639910db1586e71729a3846c206376200b778
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-12-16 19:06:18 +0500

    Conditions in Python

---
+++ if Problem_Solving/App.py
@@ -0,0 +1,103 @@
+# PROBLEM SOLVING BY SIR HITESH IN PYTHON
+# Q1
+# age=65
+
+# if age<13:
+#     print("CHILD")
+# elif age<20:
+#     print("TEENAGER")
+# elif age<60:
+#     print("ADULT")
+# else:
+#     print("SENIOR")
+
+
+# Q 02
+# age = 17
+# day = "Wednesday"
+# price = 12 if age >= 18 else 8
+# if day == "Wednesday":
+#     price -= 2
+# print("Ticket price for you is", price)
+
+# Q 03
+
+# score=115
+
+# if score >= 101:
+#     print("Please varify your grade again")
+#     exit() ye direct program ko nikal dayga
+
+# if score >=90:
+#     grade="A"
+# elif score >=80:
+#     grade="B"
+# elif score >=70:
+#     grade="C"
+# elif score >=60:
+#     grade="D"
+# else:
+#     grade="F"
+# print("Grade :",grade)
+
+
+# Q04
+# fruit = "Banana"
+# color = "Yellow"
+# if fruit == "Banana":
+#     if color == "Green":
+#         print("Unripe")
+#     elif color == "Yellow":
+#         print("Ripe")
+#     elif color == "Brown":
+#         print("Overripe")
+
+# Q05
+
+# wheather="Snowy"
+# if wheather=="Sunny":
+#     activity="Go for walk"
+# elif wheather=="Rainy":
+#     activity="Read a book"
+# elif wheather=="Snowy":
+#     activity="Build a Snowman"
+# print(activity)
+
+# distance=20
+# if distance <=3:
+#     transport="Walk"
+# elif distance<=15:
+#     transport="Bike"
+# else:
+#     transport="Cars"
+# print("Ai Recomended of",transport)
+
+
+# order_size="Medium "
+# extra_shot=True
+
+# if extra_shot:
+#     coffee=order_size +"Coffe with an extra shot"
+# else:
+#     coffee=order_size + " coffee"
+# print(coffee)
+
+# password="Zain"
+
+# if len(password)<=6:
+#     strength="Weak"
+# elif len(password)<=10:
+#     strength="Medium"
+# else:
+#    strength="Stronge"
+# print("Your Password Strength is ",strength)
+
+
+# year=2003
+
+# if (year % 400== 0) or (year % 4 == 0 and year % 100!=0):
+#     print(year ," is Leap Year")
+
+# else:
+#    print(year," is NOT Leap Year")
+
